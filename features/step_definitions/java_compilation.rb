d91d9eaf35c04d59e0af31323961351c19435c89
Luis Lavena <luislavena@gmail.com>
2009-11-24 10:13:09 +0100

    Fix detecting Java Development Kit on Windows
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- features/step_definitions/java_compilation.rb
+++ features/step_definitions/java_compilation.rb
@@ -1,9 +1,10 @@
 Given %r{^I've installed the Java Development Kit$} do
-  compilers = %w(javac)
+  compilers = %w(javac javac.exe)
   paths = ENV['PATH'].split(File::PATH_SEPARATOR)
   compiler = compilers.find do |comp|
     paths.find do |path|
-      File.exist? File.join(path, comp)
+      javac = File.join(path, comp)
+      File.exist?(javac) && File.executable?(javac)
     end
   end
   pending "Cannot locate suitable compiler in the PATH." unless compiler
